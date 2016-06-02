d415fe82be1ecab5032520d5c21fc185ac16b939
K.Stepanov <unholy.vostok@gmail.com>
2016-06-02 21:01:34 +0300

    Added #new

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,6 +5,35 @@ class CardsController < ApplicationController
   end
 
   def show
+    @card = Card.find(params[:id])
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+    if @card.save!
+      redirect_to home_path
+    else
+      render :new
+    end
+  end
+
+  def delete
+    @card = Card.find(params[:id]).delete
+    @card.save
+  end
+
+  def update
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
+
 
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,11 @@
 class Card < ApplicationRecord
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
+  validates :review_date, presence:  true
+  before_save :set_the_date
+
+  protected
+    def set_the_date
+      self.review_date = DateTime.today
+    end
 end
