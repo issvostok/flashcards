5663739f360ce292d11e463d983d1d93d1e1c604
K.Stepanov <unholy.vostok@gmail.com>
2016-06-03 20:07:57 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
+
+Card.destroy_all
+
+page.css('tr.rowB').each do |row|
+  original = row.css('td.bigLetter').text
+  array_of_words = row.css('td')[2].text.split(' ')
+  translated = array_of_words[0]
+  Card.create(original_text: original, translated_text: translated)
+end
+
+page.css('tr.rowA').each do |row|
+  original = row.css('td.bigLetter').text
+  array_of_words = row.css('td')[2].text.split(' ')
+  translated = array_of_words[0]
+  Card.create(original_text: original, translated_text: translated)
+end
+
