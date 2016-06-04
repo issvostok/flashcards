43e018f508fb2b2085815cd4fa23b696e1db84a3
K.Stepanov <unholy.vostok@gmail.com>
2016-06-04 17:45:08 +0300

    Revert "change seed logic"
    This reverts commit b1829b21f6b5a35f91feeadd39fc2b3014cd30a8.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
 
 Card.destroy_all
 
-page.css('tr').each do |row|
+page.css('tr.rowB').each do |row|
   original = row.css('td.bigLetter').text
-  translated = row.css('td')[2].text
+  array_of_words = row.css('td')[2].text.split(' ')
+  translated = array_of_words[0]
+  Card.create(original_text: original, translated_text: translated)
+end
+
+page.css('tr.rowA').each do |row|
+  original = row.css('td.bigLetter').text
+  array_of_words = row.css('td')[2].text.split(' ')
+  translated = array_of_words[0]
   Card.create(original_text: original, translated_text: translated)
 end
 
