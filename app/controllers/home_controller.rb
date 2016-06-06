9187f45cfc2f5606d6f83c9deeaca3d7d04e9269
K.Stepanov <unholy.vostok@gmail.com>
2016-06-06 19:06:45 +0300

    Add a simple training feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
+  
   def index
+    @card = Card.unreviewed
   end
+
+  def check_translation
+    @card = Card.find(params[:id])
+    if @card.check_translation(params[:answer])
+      flash[:notice] = "Correct"
+    else
+      flash[:notice] = "Incorrect"
+    end
+    redirect_to home_path
+  end
+
 end
