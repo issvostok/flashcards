937d34225e8e6efd248d3dd5857ee96bd774ffa8
K.Stepanov <unholy.vostok@gmail.com>
2016-06-08 09:01:47 +0300

    Added a seeds for flashcards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+require 'open-uri'
+page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
+
+Card.destroy_all
+
+selected_links = page.css("a[rel=nofollow]").map { |link| 'http://www.languagedaily.com' + link['href'] }
+
+selected_links.each do |link|
+  page = Nokogiri::HTML(open(link))
+  page.css('tr').each do |row|
+    if row.css('td')[2].text != ' ' || nil
+      translated = row.css('td')[2].text
+    end
+    original = row.css('td.bigLetter').text if translated
+    Card.create(original_text: original, translated_text: translated)
+  end
+end
