40463c725f43288299e2a0f30cc7235f09cd2c48
K.Stepanov <unholy.vostok@gmail.com>
2016-06-28 14:30:15 +0300

    Hotfix #2

--- app/interactors/set_current_deck.rb
+++ app/interactors/set_current_deck.rb
@@ -4,8 +4,7 @@ class SetCurrentDeck
   def call
     if context.user.decks.where(id: context.id).count > 0
       deck = Deck.find(context.id)
-      this_user = User.find(context.user)
-      this_user.decks.update(current: false)
+      context.user.decks.update(current: false)
       deck.update(current: true)
       context.notice = "Deck #{deck.title} is current now"
     else
