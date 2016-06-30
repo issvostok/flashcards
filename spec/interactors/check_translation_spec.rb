399b7caac45a8f0085267575f648de647a9e65e2
K.Stepanov <unholy.vostok@gmail.com>
2016-06-30 20:46:12 +0300

    Tests for Leintern system

--- spec/interactors/check_translation_spec.rb
+++ spec/interactors/check_translation_spec.rb
@@ -11,4 +11,10 @@ describe CheckTranslation do
     expect(interactor).to be_a_success
     expect(interactor.notice).to eq "Correct"
   end
+
+  it "#call set a currect Leitner time" do
+    interactor = CheckTranslation.call(answer: @answer, id: @card.id )
+    expect(@card.review_date).to eq(1.month.since)
+  end
+  
 end
